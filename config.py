1e3935aca37e9081e2693078c75c5dcedb60a314
CLOMDY <118122654+CLOMDY@users.noreply.github.com>
2025-09-01 13:24:57 +0530

    revert back to stable

--- config.py
+++ config.py
@@ -2,11 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "devkey123")
-
-    # Use PostgreSQL in production (Render/Heroku/etc.)
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "postgresql://testify_bkx8_user:h3nBpRHuhZW1UkBZqGmHUvyNGyHtp0Kc@dpg-d2qkctqdbo4c73c86pk0-a/testify_bkx8"
-    )
-
+    # Use SQLite (creates a file exam_portal.db in project root)
+    SQLALCHEMY_DATABASE_URI = "sqlite:///exam_portal.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
