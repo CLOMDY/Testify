a244cb5ac45647b021a75ebe267846930a65aab7
CLOMDY <118122654+CLOMDY@users.noreply.github.com>
2025-09-01 12:49:50 +0530

    changed internal url

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     # Use PostgreSQL in production (Render/Heroku/etc.)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "postgresql://username:password@host:5432/databasename"
+        "postgresql://testify_bkx8_user:h3nBpRHuhZW1UkBZqGmHUvyNGyHtp0Kc@dpg-d2qkctqdbo4c73c86pk0-a/testify_bkx8"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
